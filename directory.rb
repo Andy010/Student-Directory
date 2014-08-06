7b2cb16cfe8f71b6a393ef39a94ca7fe16095992
Andy Gates <user@Andys-MacBook-Pro.local>
2014-08-06 16:29:35 +0100

    Methods

--- directory.rb
+++ directory.rb
@@ -1,38 +1,45 @@
-
 students = [
-"Ruth Earle",
-"Andy Gates",
-"Marc Singh",
-"Faisal Aydarus",
-"Ethel Ng",
-"Kevin Daniells",
-"Maya Driver",
-"Leapold Kwok",
-"James Mcneil",
-"Jerome Pratt",
-"David Wickes",
-"Javier Silverio",
-"Elliot Lewis",
-"Ben Tillet",
-"Vincent Koch",
-"Michelle Bellard",
-"Nick Roberts",
-"Tatiana Soukiassian",
-"Mervé Silk",
-"Albert Vallverdu",
-"Lovis Schultze",
-"Chris Oatley",
-"Spke Lindsey",
-"Henry Stanley"
+	"Ruth Earle",
+	"Andy Gates",
+	"Marc Singh",
+	"Faisal Aydarus",
+	"Ethel Ng",
+	"Kevin Daniells",
+	"Maya Driver",
+	"Leapold Kwok",
+	"James Mcneil",
+	"Jerome Pratt",
+	"David Wickes",
+	"Javier Silverio",
+	"Elliot Lewis",
+	"Ben Tillet",
+	"Vincent Koch",
+	"Michelle Bellard",
+	"Nick Roberts",
+	"Tatiana Soukiassian",
+	"Mervé Silk",
+	"Albert Vallverdu",
+	"Lovis Schultze",
+	"Chris Oatley",
+	"Spke Lindsey",
+	"Henry Stanley"
 ]
 
-puts "The students of my cohort at Makers Academy"
-"------------------"
-students.each do |student|
-	puts student
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	"------------------"
 end
 
-puts "Overall, we have #{students.length} great students"
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
 
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
 
-
+print_header
+print(students)
+print_footer(students)
