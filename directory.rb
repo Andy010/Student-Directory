a792b02f1dd9726df5236a5114cccf0c3131e942
Andy Gates <user@Andys-MacBook-Pro.local>
2014-08-06 15:48:36 +0100

    Adding arrays

--- directory.rb
+++ directory.rb
@@ -1,32 +1,38 @@
-puts "The students of my cohort at Makers Academy"
 
-"------------------"
-"Ruth Earle"
-"Andy Gates"
-"Marc Singh"
-"Faisal Aydarus"
-"Ethel Ng"
-"Kevin Daniells"
-"Maya Driver"
-"Leapold Kwok"
-"James Mcneil"
-"Jerome Pratt"
-"David Wickes"
-"Javier Silverio"
-"Elliot Lewis"
-"Ben Tillet"
-"Vincent Koch"
-"Michelle Bellard"
-"Nick Roberts"
-"Tatiana Soukiassian"
-"Mervé Silk"
-"Albert Vallverdu"
-"Lovis Schultze"
-"Chris Oatley"
-"Spke Lindsey"
+students = [
+"Ruth Earle",
+"Andy Gates",
+"Marc Singh",
+"Faisal Aydarus",
+"Ethel Ng",
+"Kevin Daniells",
+"Maya Driver",
+"Leapold Kwok",
+"James Mcneil",
+"Jerome Pratt",
+"David Wickes",
+"Javier Silverio",
+"Elliot Lewis",
+"Ben Tillet",
+"Vincent Koch",
+"Michelle Bellard",
+"Nick Roberts",
+"Tatiana Soukiassian",
+"Mervé Silk",
+"Albert Vallverdu",
+"Lovis Schultze",
+"Chris Oatley",
+"Spke Lindsey",
 "Henry Stanley"
+]
 
-print "Overall, we have " #{student_count} " great students"
+puts "The students of my cohort at Makers Academy"
+"------------------"
+students.each do |student|
+	puts student
+end
+
+puts "Overall, we have #{students.length} great students"
 
 
 
